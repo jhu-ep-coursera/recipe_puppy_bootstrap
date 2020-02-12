c20141f1737f76cd82c5d3228b384abe9af11f6c
Kalman Hazins <kalmanh@gmail.com>
2020-02-12 00:04:37 -0500

    Temporary workaround for recipepuppy API

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,23 @@
 class Recipe
     include HTTParty
         
-    base_uri "http://'www.recipepuppy.com'/api"
+    base_uri 'http://www.recipepuppy.com/api'
     default_params onlyImages: 1
-    format :json
 
-    def self.for (keyword)
-        get("", query: {q: keyword})["results"]
+    ## ENABLE THE FOLLOWING LINE ONCE recipepuppy.com fully working...
+    # format :json
+
+    def self.for ( keyword = 'chocolate' )
+        # get('', query: { q: keyword })['results']
+        ## Once recipepuppy.com fully working, you can
+        ## uncomment the line above and comment out the line below
+        temporary_work_around_to_account_for_error_in_httparty(keyword)
+    end
+
+    def self.temporary_work_around_to_account_for_error_in_httparty(keyword)
+        response_text = get('', query: { q: keyword })
+        response_text.sub!(/<!DOCTYPE.*/m, '')
+        require 'json'
+        JSON.parse(response_text)['results']
     end
 end
