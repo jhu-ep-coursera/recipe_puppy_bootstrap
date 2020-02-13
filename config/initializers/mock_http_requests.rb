d175631a90b013b6d802f6c34c4be926e505e6e4
Kalman Hazins <kalmanh@gmail.com>
2020-02-13 00:58:56 -0500

    Determine response dynamically based on regexing the request

--- config/initializers/mock_http_requests.rb
+++ config/initializers/mock_http_requests.rb
@@ -4,12 +4,9 @@ if Rails.configuration.use_mocks_please
 
     WebMock.enable!
 
-    stub_request(:get, /chocolate/).
-        to_return(body: File.new("#{Rails.root}/spec/mocks/chocolate.json"), status: 200)
-
-    stub_request(:get, /amarula/).
-        to_return(body: File.new("#{Rails.root}/spec/mocks/amarula.json"), status: 200)
-    
-    stub_request(:get, /bread/).
-        to_return(body: File.new("#{Rails.root}/spec/mocks/bread.json"), status: 200)
+    stub_request(:get, /recipepuppy\.com\/api\//).
+        to_return { |request|
+            matched_result = request.uri.query.match(/q=(.*)/)
+            { body: File.new("#{Rails.root}/spec/mocks/#{matched_result[1]}.json"), status: 200 }
+        }             
 end
